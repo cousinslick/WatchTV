9fab0ed0a6cc11dd4e76792fdabf0e8daa25b686
cousinslick <112375209+cousinslick@users.noreply.github.com>
2023-04-27 15:31:53 -0700

    WatchTV 0.34.3
    Update Get-TegnaDVR

--- WatchTV/Get-TegnaDVR.ps1
+++ WatchTV/Get-TegnaDVR.ps1
@@ -3,11 +3,12 @@ function Get-TegnaDVR
   param (
     [Parameter(Mandatory = $true)][string] $Name,
     [Parameter(Mandatory = $true)][string] $Id,
-    [Parameter(Mandatory = $true)][string] $Key
+    [Parameter(Mandatory = $true)][string] $Key,
+    [string] $Qty = "10"
   )
 
   $ua = "$([uri]::EscapeDataString($Name))/1 CFNetwork/1399 Darwin/22.1.0"
-  $magicUrl = "https://api.tegnadigital.com/mobile/content-ro/getContentListV2ForNativeUx/$($Id)/video/10/news/dvr?subscription-key=$($Key)"
+  $magicUrl = "https://api.tegnadigital.com/mobile/content-ro/getContentListV2ForNativeUx/$($Id)/video/$($Qty)/news/dvr?subscription-key=$($Key)"
   $streams = Invoke-RestMethod -Method Get -Uri $magicUrl -UserAgent $ua
 
   $result = [System.Collections.ArrayList]::new()
