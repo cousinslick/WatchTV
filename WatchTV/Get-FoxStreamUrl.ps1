ea0bfd67d2499e6880f2995dbf704e006d6ee2ac
cousinslick <112375209+cousinslick@users.noreply.github.com>
2026-02-09 06:16:01 -0800

    Update Get-FoxStreamUrl.ps1

--- WatchTV/Get-FoxStreamUrl.ps1
+++ WatchTV/Get-FoxStreamUrl.ps1
@@ -43,11 +43,50 @@ function Get-FoxStreamUrl
     # And then build our request URL. This magic URL was obtained from dev tools in the response to https://access.mp.lura.live/anvacks/...
     # {"api":{"video":"https://tkx.mp.lura.live/rest/v2/mcp/video/{{VIDEO_ID}}?anvack={{ANVACK}}"}}
     $videoApiUrl = "https://tkx.mp.lura.live/rest/v2/mcp/video/$($videoId)?anvack=$($anvack)"
-    $result = Invoke-WebRequest -Uri $videoApiUrl -UseBasicParsing -UserAgent (Get-UA)
+    try
+    {
+      $result = Invoke-WebRequest -Uri $videoApiUrl -UseBasicParsing -UserAgent (Get-UA) -ErrorAction SilentlyContinue
 
-    # The call to the video API returns a JSON payload wrapped in a anvatoVideoJSONLoaded callback.
-    # Strip off the callback and parse the JSON result to get the m3u8 URL
-    $embed_url = (($result.Content -replace "^.+?\(" -replace "\)$" | ConvertFrom-Json).published_urls | Where-Object -Property format -like "m3u8*").embed_url
-    return $embed_url
+      # The call to the video API returns a JSON payload wrapped in a anvatoVideoJSONLoaded callback.
+      # Strip off the callback and parse the JSON result to get the m3u8 URL
+      $embed_url = (($result.Content -replace "^.+?\(" -replace "\)$" | ConvertFrom-Json).published_urls | Where-Object -Property format -like "m3u8*").embed_url
+      return $embed_url
+    }
+    catch {}
+
+    # In Dec. 2025, we observed a change in Fox O&O live streaming. Instead of a call to a magic lura.live URL,
+    # call a new watchlive API endpoint with $videoId in a POST body to fetch the stream url
+    if ($null -eq $result)
+    {
+      $watchLiveEndpoint = "https://prod.api.digitalvideoplatform.com/fts/v3.0/watchlive"
+      $wlBody = @{
+        asset = @{
+          id = $videoId
+        }
+        stream = @{
+          type = "live"
+        }
+        device = @{
+          capabilities = @()
+          width = (Get-Random -Minimum 863 -Maximum 1920)
+          height = (Get-Random -Minimum 647 -Maximum 1260)
+          os = "Windows"
+          osv = "10"
+        }
+        ad = @{
+          did = $null
+        }
+        debug = @{
+          traceId = $null
+        }
+        privacy = @{
+          us = "1YYN"
+          lat = $true
+        }
+      } | ConvertTo-Json -Compress
+      $result = Invoke-WebRequest -Uri $watchLiveEndpoint -UserAgent (Get-UA) -Method Post -Body $wlBody -ContentType "application/json"
+      $response = $result.Content | ConvertFrom-Json
+      return $response.stream.playbackUrl
+    }
   }
 }
